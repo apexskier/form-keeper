799a5677cdabf21bb3b1913f9fcb266a5991be0d
Cameron Little <cameron@camlittle.com>
2024-12-07 15:21:30 -0700

    ios app UI

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -15,66 +15,131 @@ extension Text {
     }
 }
 
-struct ContentView: View {
-    @State var extensionState: SFSafariExtensionState? = nil
+struct NumberedView<Content>: View where Content: View {
+    var index: Int
+
+    @ViewBuilder var content: () -> Content
 
     var body: some View {
-        VStack(spacing: 20) {
+        HStack(alignment: .firstTextBaseline) {
+            Image(systemName: "\(index).square")
+            content()
+        }
+    }
+}
+
+struct ContentView: View {
+    #if os(macOS)
+        @State var extensionState: SFSafariExtensionState? = nil
+    #endif
+
+    @ScaledMetric var scale = 1.0
+
+    var body: some View {
+        #if os(macOS)
+            innerBody
+        #else
+            ScrollView {
+                innerBody
+            }
+        #endif
+    }
+
+    var innerBody: some View {
+        VStack(spacing: 40) {
             Image("Logo")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 120, height: 120)
+
             VStack(spacing: 8) {
                 Text(
                     "FormKeeper perserves content you've entered into forms in case of accidental refresh or page close."
                 )
                 .customText()
-                if let extensionState {
-                    if extensionState.isEnabled {
-                        Text(
-                            "FormKeeper’s extension is currently on. You can turn it off in Safari Extensions preferences."
-                        )
-                        .customText()
+                #if os(macOS)
+                    if let extensionState {
+                        if extensionState.isEnabled {
+                            Text(
+                                "FormKeeper’s extension is currently on. You can turn it off in Safari Extensions preferences."
+                            )
+                            .customText()
+                        } else {
+                            Text(
+                                "FormKeeper’s extension is currently off. You can turn it on in Safari Extensions preferences."
+                            )
+                            .customText()
+                        }
                     } else {
                         Text(
-                            "FormKeeper’s extension is currently off. You can turn it on in Safari Extensions preferences."
+                            "You can turn on FormKeeper’s extension in Safari Extensions preferences."
                         )
                         .customText()
                     }
-                } else {
-                    Text(
-                        "You can turn on FormKeeper’s extension in Safari Extensions preferences."
-                    )
-                    .customText()
-                }
+                #endif
             }
             .lineLimit(nil)
             .font(.body)
             .frame(maxWidth: 400)
-            .fixedSize()
+            #if os(macOS)
+                .fixedSize()
+            #endif
 
-            Button {
-                SFSafariApplication.showPreferencesForExtension(
-                    withIdentifier: extensionBundleIdentifier
-                ) { (error) in
-                    NSLog("Error \(String(describing: error))")
+            #if os(macOS)
+                Button {
+                    SFSafariApplication.showPreferencesForExtension(
+                        withIdentifier: extensionBundleIdentifier
+                    ) { (error) in
+                        NSLog("Error \(String(describing: error))")
+                    }
+                } label: {
+                    Text("Open Safari Extensions Preferences")
                 }
-            } label: {
-                Text("Open Safari Extensions Preferences")
-            }
+            #else
+                VStack(spacing: 20) {
+                    Text(
+                        "You can turn on FormKeeper’s extension in Safari Extensions preferences."
+                    )
+                    .customText()
+                    Text("We recommend enabling everywhere:")
+                        .customText()
+                    VStack(spacing: 8) {
+                        NumberedView(index: 1) {
+                            Text(
+                                "Go to **Settings** > **Apps** > **Safari** > **Extensions** > **\(Bundle.main.infoDictionary!["CFBundleName"] as! String)**"
+                            ).customText()
+                        }
+                        NumberedView(index: 2) {
+                            Text("Turn on **Allow Extension**")
+                                .customText()
+                        }
+                        NumberedView(index: 3) {
+                            Text("Under **Permissions**, tap **All Websites**")
+                                .customText()
+                        }
+                        NumberedView(index: 4) {
+                            Text("Select **Ask**")
+                                .customText()
+                        }
+                    }
+                }
+                .font(.body)
+            #endif
         }
         .padding()
-        .onAppear {
-            SFSafariExtensionManager.getStateOfSafariExtension(
-                withIdentifier: extensionBundleIdentifier
-            ) { (state, error) in
-                extensionState = state
-                if let error = error {
-                    print("Error getting extension state: \(error)")
-                    return
+        #if os(macOS)
+            .onAppear {
+                SFSafariExtensionManager.getStateOfSafariExtension(
+                    withIdentifier: extensionBundleIdentifier
+                ) { (state, error) in
+                    extensionState = state
+                    if let error = error {
+                        print("Error getting extension state: \(error)")
+                        return
+                    }
                 }
             }
-        }
+        #endif
     }
 }
 
--- App/app.swift
+++ App/app.swift
@@ -5,7 +5,7 @@ let extensionBundleIdentifier = "com.camlittle.form-keeper.Extension"
 @main
 struct MyMain {
     static func main() {
-        if #available(macOS 13.0, *) {
+        if #available(macOS 13.0, iOS 17.0, *) {
             ModernApp.main()
         } else {
             OldApp.main()
@@ -13,21 +13,42 @@ struct MyMain {
     }
 }
 
-@available(macOS 13.0, *)
-struct ModernApp: App {
-    var body: some Scene {
-        Window("FormKeeper", id: "main") {
-            ContentView()
-        }
+#if os(macOS)
+    @available(macOS 13.0, *)
+    struct ModernApp: App {
+        var body: some Scene {
+            Window("FormKeeper", id: "main") {
+                ContentView()
+            }
             .windowResizability(.contentSize)
             .defaultPosition(.center)
-    }
-}
-
-struct OldApp: App {
-    var body: some Scene {
-        WindowGroup(id: "main") {
-            ContentView()
         }
     }
-}
+
+    struct OldApp: App {
+        var body: some Scene {
+            WindowGroup(id: "main") {
+                ContentView()
+            }
+        }
+    }
+#else
+    @available(iOS 17.0, *)
+    struct ModernApp: App {
+        var body: some Scene {
+            WindowGroup {
+                ContentView()
+                    .background()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+        }
+    }
+
+    struct OldApp: App {
+        var body: some Scene {
+            WindowGroup {
+                ContentView()
+            }
+        }
+    }
+#endif
