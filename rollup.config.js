2679711cb8bc33df65056f75baa822891f600f2a
Cameron Little <cameron@camlittle.com>
2024-12-10 09:30:54 -0700

    use rollup to compile typescript for sourcemap support

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,17 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import replace from "@rollup/plugin-replace";
+import typescript from "@rollup/plugin-typescript";
 
 export default [
   {
-    input: ["Extension/dst/popup/index.js"],
+    input: ["Extension/src/popup/index.tsx"],
     output: {
       sourcemap: true,
       file: "Extension/Resources/popup.js",
     },
     plugins: [
+      typescript(),
       nodeResolve({ browser: true }),
       replace({
         "process.env.NODE_ENV": JSON.stringify("development"),
@@ -18,18 +20,18 @@ export default [
     ],
   },
   {
-    input: ["Extension/dst/background.js"],
+    input: ["Extension/src/background.ts"],
     output: {
       dir: "Extension/Resources",
     },
-    plugins: [nodeResolve()],
+    plugins: [typescript(), nodeResolve()],
   },
   {
-    input: "Extension/dst/content.js",
+    input: "Extension/src/content.ts",
     output: {
       dir: "Extension/Resources",
       format: "iife",
     },
-    plugins: [nodeResolve()],
+    plugins: [typescript(), nodeResolve()],
   },
 ];
