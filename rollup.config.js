904524f0d9aed69e6977ad94885394992667b0a8
Cameron Little <cameron@camlittle.com>
2024-12-10 09:36:43 -0700

    Configure dev/prod from xcode to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,22 @@ import commonjs from "@rollup/plugin-commonjs";
 import replace from "@rollup/plugin-replace";
 import typescript from "@rollup/plugin-typescript";
 
+let development = process.env["CONFIGURATION"] === "Debug";
+
 export default [
   {
     input: ["Extension/src/popup/index.tsx"],
     output: {
-      sourcemap: true,
+      sourcemap: development,
       file: "Extension/Resources/popup.js",
     },
     plugins: [
       typescript(),
       nodeResolve({ browser: true }),
       replace({
-        "process.env.NODE_ENV": JSON.stringify("development"),
+        "process.env.NODE_ENV": JSON.stringify(
+          development ? "development" : "production"
+        ),
       }),
       commonjs(),
     ],
@@ -22,6 +26,7 @@ export default [
   {
     input: ["Extension/src/background.ts"],
     output: {
+      sourcemap: development,
       dir: "Extension/Resources",
     },
     plugins: [typescript(), nodeResolve()],
@@ -29,6 +34,7 @@ export default [
   {
     input: "Extension/src/content.ts",
     output: {
+      sourcemap: development,
       dir: "Extension/Resources",
       format: "iife",
     },
